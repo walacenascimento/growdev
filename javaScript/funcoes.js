4e8ce2e4c958fecfe3773f82c899f35f1cb18939
Walace Nascimento <wlacens@hotmail.com>
2026-02-23 10:18:10 -0300

    Removendo todos os arquivos da branch main e mantendo a branch

--- javaScript/funcoes.js
+++
@@ -1,8 +0,0 @@
-// Declaração de função
-function teste() {
-    console.log(`Teste de função`);
-    alert(`Testando a funçao!`)
-}
-
-// Chamada de função
-teste()
--- javaScript/soma.js
+++
@@ -1,6 +0,0 @@
-function soma(a, b) {
-    result = a + b;
-    console.log(`A soma é: ${result}`);
-}
-
-soma(5, 4)
