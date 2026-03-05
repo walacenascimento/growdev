85a9f45d529ead2ebbd31ff0b37fa85468dac85b
Walace Nascimento <wlacens@hotmail.com>
2026-03-05 20:07:18 -0300

    Efetuando a configuração do servidor e passando a rota de teste /get

--- apiGrowdev/src/index.js
+++ apiGrowdev/src/index.js
@@ -1,1 +1,28 @@
-console.log('Testando o servidor');
+import express from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT
+
+// Criando as rotas
+app.listen(port, ()=>{
+    console.log(`"Servidor rodando na porta ${port}"` );
+});
+
+// listando algo
+// app.get("/teste", (req, res)=>{
+//     console.log(`Hello World teste!`);
+//     res.send(`Hello World!`)
+// });
+
+// também podemos retornar no send um arquivo JSON com várias iformações
+app.get("/teste", (req, res)=>{
+    console.log(`Hello World teste!`);
+    res.send({
+        OK: true,
+        message: "Hello World, rota executada com sucesso!"
+    })
+});
