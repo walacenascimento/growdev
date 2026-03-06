c6a92e3928a69d48a99c5762e495bf5431158eb7
Walace Nascimento <wlacens@hotmail.com>
2026-03-05 22:48:30 -0300

    Finalizando a aula de implementação das rotas

---
+++ apiGrowdev/src/dados.js
@@ -0,0 +1,18 @@
+import {randomUUID} from 'crypto';
+
+export const growdevers = [
+    {
+        id: randomUUID(),
+        nome: "Walace",
+        email: 'walace@gmail.com',
+        idade: 35,
+        matriculado: true
+    },
+    {
+        id: randomUUID(),
+        nome: "Liz",
+        email: 'liz@gmail.com',
+        idade: 15,
+        matriculado: false
+    },
+]
--- apiGrowdev/src/index.js
+++ apiGrowdev/src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import {growdevers} from './dados.js';
+import {randomUUID} from 'crypto';
 dotenv.config();
 
 const app = express();
@@ -8,9 +10,91 @@ app.use(express.json());
 const port = process.env.PORT
 
 // Criando as rotas
+// GET - growdevers
+app.get("/growdevers", (req, res) =>{const {idade} = req.query;
+    let dados = growdevers
+    
+    if(idade){
+        dados = dados.filter(item => item.idade === Number(idade));
+    }
+
+    res.status(200).send({
+        ok: true,
+        mensagem: "Growdevers listado com sucesso!",
+        // dados: growdevers
+        dados
+    });
+});
+
+// GET-ID - Buscando um growdever pelo id
+app.get("/growdevers/:id", (req, res) =>{
+    // entrada
+    //const params = req.params // ou de forma desestruturada assim: const {id} = req.params
+    const {id} = req.params
+
+    // processamento
+    const growdever = growdevers.find((item) => item.id === id);
+    // validado se o growdever existe
+    if(!growdever){
+        return res.status(404).send({
+            OK: false,
+            mensagem: "Growdev não encontrado!"
+        });
+    }
+
+//     // GET - usando query params (opcional);
+//     app.get("/growdevers", (req, res) =>{
+//         const {idade} = req.query;
+
+//         let dados = growdevers
+//         if(idade){
+//             dados = dados.filter(item => item.idade === Number(idade));
+//         }
+
+//         res.status(200).send({
+//         ok: true,
+//         mensagem: "Growdevers listado com sucesso!",
+//         dados
+//     });
+// });
+
+    // saida
+    res.status(200).send({
+        OK: true,
+        mensagem: "Growdev obtido com sucesso!",
+        dados: growdever
+    })
+
+})
+
+// POST - Cadastrando um novo Growdever
+app.post("/growdevers", (req, res) => {
+    // entrada de dados
+    const body = req.body;
+    const novoGrowdevers = {
+        id: randomUUID(),
+        nome: body.nome,
+        email: body.email,
+        idade: body.idade,
+        matriculado: body.matriculado
+    }
+    // Processando - inserido os dados de entrada no banco de dados - no nosso caso na lista "dados".
+    growdevers.push(novoGrowdevers);
+
+    // Saída de dados
+    res.status(201).send({
+        OK: true,
+        mensagem: "Growdevers cadastrado com sucesso!",
+        dados: growdevers
+    });
+});
+
+
+
 app.listen(port, ()=>{
     console.log(`"Servidor rodando na porta ${port}"` );
 });
+
 
 // listando algo
 // app.get("/teste", (req, res)=>{
@@ -19,10 +103,10 @@ app.listen(port, ()=>{
 // });
 
 // também podemos retornar no send um arquivo JSON com várias iformações
-app.get("/teste", (req, res)=>{
-    console.log(`Hello World teste!`);
-    res.send({
-        OK: true,
-        message: "Hello World, rota executada com sucesso!"
-    })
-});
+// app.get("/teste", (req, res)=>{
+//     console.log(`Hello World teste!`);
+//     res.send({
+//         OK: true,
+//         message: "Hello World, rota executada com sucesso!"
+//     })
+// });
